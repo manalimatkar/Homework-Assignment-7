f974050e1e0a460f46938a023673ae123bd20edb
manaliraleraskar <manali.raleraskar@gmail.com>
2016-09-14 17:40:11 -0500

    Data Push To Firework

--- assets/js/trainschedule.js
+++ assets/js/trainschedule.js
@@ -1,136 +1,51 @@
-// Initialize Firebase
-
+ // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyDe13RnvAnEgoeGGtvbtvMjmCmt86DZmvE",
-    authDomain: "codersbay-c737e.firebaseapp.com",
-    databaseURL: "https://codersbay-c737e.firebaseio.com",
-    storageBucket: "codersbay-c737e.appspot.com",
+    apiKey: "AIzaSyD6hANL5cQyrc1vUsqoC4BsBFA_6-phjUU",
+    authDomain: "trainsch-9b29e.firebaseapp.com",
+    databaseURL: "https://trainsch-9b29e.firebaseio.com",
+    storageBucket: "trainsch-9b29e.appspot.com",
+    messagingSenderId: "278163231911"
   };
   firebase.initializeApp(config);
-
-
-// Create a variable to reference the database
+  
 
 var database = firebase.database();
 
-// Initial Values
-var initialBid = 0;
-var initialBidder = "No one :-("
+// Intial Variables
 
-var highPrice = initialBid;
-var highBidder = initialBidder;
+$("#addTrain").on("click", function() {
 
-// --------------------------------------------------------------
+	//Get user input
+	var trainName = $('#trainName').val().trim();
+	var trainDestination = $('#trainDestination').val().trim();
+	var firstTrainTime = $('#trainTimeHour').val().trim() + ":" + $('#trainTimeMin').val().trim();
+	var trainFrequency = $('#trainFrequency').val().trim();
 
-// 
-// At the initial load, get a snapshot of the current data.
-database.ref().on("value", function(snapshot) {
+	console.log(trainName);
+	console.log(trainDestination);
+	console.log(firstTrainTime);
+	console.log(trainFrequency);
 
-	// If Firebase has a highPrice and highBidder stored (first case)
-	if (snapshot.child("highBidder").exists() && snapshot.child("highPrice").exists()) {
-
-		// Set the initial variables for highBidder equal to the stored values.
-         
-         highBidder = snapshot.val().highBidder;
-         highPrice =  snapshot.val().highPrice;
-
-		// Change the HTML to reflect the initial value
-         
-        $("#highestBidder").html(highBidder);
-        $("#highestPrice").html(highPrice);
-
-
-		// Print the initial data to the console.
-		console.log("snapshot.val().highBidder"+ snapshot.val().highBidder);
-		console.log("snapshot.val().highPrice"+ snapshot.val().highPrice);
+	// Creates local "temporary" object for holding employee data
+	var newTrain = {
+		name:  trainName,
+		destination: trainDestination,
+		firstTrainAt: firstTrainTime,
+		frequency: trainFrequency
 	}
 
-	// Keep the initial variables for highBidder equal to the initial values
-	else{
+	// Uploads employee data to the database
+	database.ref().push(newTrain);
 
-		// Change the HTML to reflect the initial value
-
-
-		highPrice = initialBid;
-		highBidder = initialBidder;
-
-		// Print the initial data to the console.
-		$("#highestBidder").html(highBidder);
-        $("#highestPrice").html(highPrice);
-
-
-
-
-	}
-
-
-
-// If any errors are experienced, log them to console. 
-}, function (errorObject) {
-
-  	console.log("The read failed: " + errorObject.code);
-
-});
-
-// --------------------------------------------------------------
-
-// Whenever a user clicks the click button
-$("#submitBid").on("click", function() {
-
-	// Get the input values
-
-	var bidderName = $("#bidderName").val().trim();
-	var bidderPrice = parseInt($("#bidderPrice").val().trim());
-
-	// Log the Bidder and Price (Even if not the highest)
-
-
-
-	if(bidderPrice > highPrice) {
-
-		// Alert 
-		alert("You are now the highest bidder.");
-
-
-        //clear input fields
-
-        $("#bidderPrice").val("");
-        $("#bidderName").val("");
-
-
-		// Save the new price in Firebase
-		database.ref().set({
-			highBidder: bidderName,
-			highPrice: bidderPrice
-		});
-
-
-		// Log the new High Price
-
-		console.log(highPrice);
-
-		// Store the new high price and bidder name as a local variable (could have also used the firebase variable)
-
-		highPrice = bidderPrice;
-		highBidder = bidderName;
-
-
-		// Change the HTML to reflect the new high price and bidder
-
-		$("#highestBidder").html(bidderName);
-        $("#highestPrice").html(bidderPrice);
-
-
-	}
-
-	else{
-
-		// Alert
-		alert("Sorry that bid is too low. Try again.");	
-	}
-
-	// Return False to allow "enter"
 	return false;
+
 });
 
+database.ref().on("child_added", function(snapshot, prevChildKey) {
+  var newTrain = snapshot.val();
+    console.log("Previous Post ID: " + prevChildKey);
 
+   $("#trainList").append("<tr><td>" + newTrain.name+ "</td><td>" + newTrain.destination +"</td><td>" + newTrain.frequency + "</td><td>" + "" +"</td><td>" + "" +"</td></tr>");
+
+ });
+ 
